c5d2a198ecb2816b1a2fbb2a74a5186cc7b8e81e
redvec <redvectoria@outlook.com>
2023-05-23 10:14:07 -0500

    que locura es esto

---
+++ functions.js
@@ -0,0 +1,9 @@
+// Check if the current character is a comma
+// Check if the current character is a comma
+// and add a line break after it
+// Add the next character to the code element
+// Increment the character index
+// Check if all characters have been typed
+// Clear the interval to stop typing
+// Adjust the typing speed here (in milliseconds)
+// Start the typewriter effect for each code element
