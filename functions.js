c76bb47ba45002d150f2e67fd8bccaba205ede39
redvec <redvectoria@outlook.com>
2023-05-25 06:15:38 -0500

    errores aceptable

--- functions.js
+++ functions.js
@@ -7,3 +7,28 @@
 // Clear the interval to stop typing
 // Adjust the typing speed here (in milliseconds)
 // Start the typewriter effect for each code element
+
+// const textElement = document.getElementById('serenos');
+// const text = textElement.innerHTML;
+// textElement.innerHTML = '';
+
+// for (const letter of text) {
+//   const letra = document.createElement('t');
+//   span.textContent = letter;
+//   textElement.appendChild(sp);
+// }
+
+
+// const textElement = document.getElementById('serenos');
+// const text = textElement.innerHTML;
+// textElement.innerHTML = '';
+
+// const createTypewriterDelayElement = (letter) => {
+//   const typewriterDelayElement = document.createElement('p');
+//   typewriterDelayElement.textContent = letter;
+//   typewriterDelayElement.classList.add('typewriter-delay');
+//   return typewriterDelayElement;
+// };
+
+// const typewriterDelayElements = Array.from(text).map(createTypewriterDelayElement);
+// typewriterDelayElements.forEach((element) => textElement.appendChild(element));
