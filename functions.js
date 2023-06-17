139d5ab819d0024c12fc1a8b8b9e46015564e8e5
redvec <redvectoria@outlook.com>
2023-06-17 07:43:07 -0500

    que locura esto

--- functions.js
+++ functions.js
@@ -1,22 +1,39 @@
 function navigateToPreviousChapter() {
     navigateToChapter(calculateChapterNumber() - 1);
-}
-
-function navigateToNextChapter() {
+  }
+  
+  function navigateToNextChapter() {
     navigateToChapter(calculateChapterNumber() + 1);
-}
-
-function calculateChapterNumber() {
-    var currentChapter = document.querySelector(".libromitir").textContent;
+  }
+  
+  function calculateChapterNumber() {
+    var currentChapter = document.querySelector(".libromitir h2.cabeceras").textContent;
     var chapterNumber = parseInt(currentChapter.substring(0, 2));
     return chapterNumber;
-}
-
-function navigateToChapter(chapterNumber) {
+  }
+  
+  
+    var currentChapter = libromitirElement.textContent;
+    if (!currentChapter) {
+      console.error("Text content of 'libromitir' element is null or empty.");
+      return 0; // Return a default value or handle the error accordingly
+    }
+  
+    var chapterNumber = parseInt(currentChapter.substring(0, 2));
+    if (isNaN(chapterNumber)) {
+      console.error("Unable to parse chapter number from text content.");
+      return 0; // Return a default value or handle the error accordingly
+    }
+  
+    return chapterNumber;
+  }
+  
+  function navigateToChapter(chapterNumber) {
     var chapterFolder = chapterNumber.toString().padStart(2, '0') + "__";
     var chapterURL = chapterFolder + "chapter.html";
     
     // Perform the navigation to the specified chapter URL
     // Replace the following line with your actual code
     console.log("Navigating to chapter: " + chapterURL);
-}
+  }
+  
